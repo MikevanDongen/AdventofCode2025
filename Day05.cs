9571cf359b86867045c36a9fd408d7cd65f13a0e
Mike van Dongen <github@mikevandongen.nl>
2025-12-05 08:01:31 +0100

    Day05 (part 2)

--- Day05.cs
+++ Day05.cs
@@ -26,7 +26,29 @@ public class Day05(ITestOutputHelper output)
         Assert.Equal(517, numberOfFreshIngredients);
     }
 
-    private long GetNumberOfFreshIngredients(string inputFilename)
+    [Fact]
+    public void Part2_Demo()
+    {
+        var inputFilename = $"inputs/{GetType().Name.ToLower()}-demo.txt";
+        var numberOfFreshIngredients = GetNumberOfFreshIngredients2(inputFilename);
+
+        output.WriteLine($"Number of fresh ingredients: {numberOfFreshIngredients}");
+
+        Assert.Equal(14, numberOfFreshIngredients);
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        var inputFilename = $"inputs/{GetType().Name.ToLower()}.txt";
+        var numberOfFreshIngredients = GetNumberOfFreshIngredients2(inputFilename);
+
+        output.WriteLine($"Number of fresh ingredients: {numberOfFreshIngredients}");
+
+        Assert.Equal(336173027056994, numberOfFreshIngredients);
+    }
+
+    private static long GetNumberOfFreshIngredients(string inputFilename)
     {
         var lines = File.ReadLines(inputFilename).ToList();
         var splitIndex = lines.IndexOf(string.Empty);
@@ -37,5 +59,39 @@ public class Day05(ITestOutputHelper output)
             .Where(i => freshRanges.Any(r => i >= r[0] && i <= r[1]));
 
         return freshIngredients.Count();
+    }
+
+    private static long GetNumberOfFreshIngredients2(string inputFilename)
+    {
+        var lines = File.ReadLines(inputFilename).ToList();
+        var splitIndex = lines.IndexOf(string.Empty);
+
+        var freshRanges = lines[..splitIndex]
+            .Select(r => r.Split('-').Select(long.Parse).ToArray())
+            .OrderBy(r => r[0]);
+
+        var numberOfFreshIngredients = 0L;
+        var lastHigh = 0L;
+
+        foreach (var freshRange in freshRanges)
+        {
+            if (freshRange[1] <= lastHigh)
+            {
+                continue;
+            }
+
+            if (freshRange[0] <= lastHigh)
+            {
+                numberOfFreshIngredients += freshRange[1] - lastHigh;
+            }
+            else
+            {
+                numberOfFreshIngredients += freshRange[1] - freshRange[0] + 1;
+            }
+
+            lastHigh = freshRange[1];
+        }
+
+        return numberOfFreshIngredients;
     }
 }
